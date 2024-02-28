ba2150d2b0e7f771931b062b5f2aabdd34296815
jenny <dooly3392@gmail.com>
2024-02-29 00:41:59 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 import LayoutAnimation from "./LayoutAnimation";
-=======
-import Slider from "./Slider";
->>>>>>> 42600c6d88ed95a24182acb046bd2dfa4762d2dc
-
 
 function App() {
   return (
-<<<<<<< HEAD
     <LayoutAnimation />
-=======
-    <Slider />
->>>>>>> 42600c6d88ed95a24182acb046bd2dfa4762d2dc
   );
 }
 
