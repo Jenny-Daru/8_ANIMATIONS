457eb4dc1e36d5e7cea44de6266dfe79d4ffdc31
jenny <dooly3392@gmail.com>
2024-02-28 23:26:59 +0900

    AnimatePresence 사용해 보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
+import AnimatePre from "./AnimatePre";
 
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  position: absolute;
-  top: 100px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
 
 function App() {
   return (
-    <Wrapper>
-      <Box />
-    </Wrapper>
+    <AnimatePre />
   );
 }
 
