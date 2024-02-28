c53de12b0f88130656502dc4b91436f4d0bc0c88
jenny <dooly3392@gmail.com>
2024-02-28 23:06:04 +0900

    중간 정리: Animations.tsx로 코드 이동

---
+++ src/Animations.tsx
@@ -0,0 +1,187 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import {
+  motion,
+  useMotionValue,
+  useScroll,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
+
+const Wrapper = styled(motion.div)`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box1 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Box2 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Box3 = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  margin: 10px;
+  background-color: white;
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const BiggerBox = styled.div`
+  width: 450px;
+  height: 450px;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  stroke: white;
+  stroke-width: 3;
+`;
+
+const box1Vars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
+const box2Vars = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const box3Vars = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+};
+
+const box4Vars = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+};
+
+const circleVars = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const svgVars = {
+  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
+  end: {
+    pathLength: 1,
+    fill: "rgba(255, 255, 255, 1)",
+  },
+};
+
+function Animations() {
+  return (
+    <Wrapper>
+      <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <motion.path
+          variants={svgVars}
+          initial="start"
+          animate="end"
+          transition={{
+            default: { duration: 5 },
+            fill: { duration: 1, delay: 3 },
+          }}
+          d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"
+        />
+      </Svg>
+    </Wrapper>
+  );
+}
+/* 
+function Animations() {
+  const biggerBoxRef = useRef(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(102, 183, 233), rgb(51, 74, 206))",
+      "linear-gradient(135deg, rgb(91, 195, 132), rgb(186, 213, 74))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+  useEffect(() => {
+    x.on("change", () => console.log(x.get()));
+  }, [x]);
+  return (
+    <Wrapper style={{ background: gradient }}>
+      <Box1 variants={box1Vars} initial="start" animate="end" />
+      <Box2 variants={box2Vars} initial="start" animate="end">
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+        <Circle variants={circleVars} />
+      </Box2>
+      <Box3 variants={box3Vars} whileHover="hover" whileTap="click" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box3
+          drag
+          dragSnapToOrigin
+          dragConstraints={biggerBoxRef}
+          variants={box4Vars}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
+      <Box3 style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
+    </Wrapper>
+  );
+} */
+
+export default Animations;
